b555ee20a65349966f31def366c66b3b69be1238
ZACGIL <zac.gilfoyle@gmail.com>
2024-04-09 14:20:18 +1000

    added delete button for tasks and removed add task dialogue upon submitting form

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,7 +9,11 @@ const taskTitle = $("#title");
 const taskDesc = $("#desc");
 const taskDate = $("#date");
 const toDoContainer = $("#todo-cards");
-// variables
+const inProgContainer = $("#in-progress-cards");
+const doneContainer = $("#done-cards");
+const swimLanes = $(".lane");
+const dragTasks = $(".ui-draggable");
+// Variables
 let uniqueID = 0;
 let toDoArr = [];
 let inProgressArr = [];
@@ -35,58 +39,69 @@ function renderTaskList(container, array) {
     }
 }
 
-function renderCard(container, array) {
+function renderCard(container, task) {
+    // Create HTML elements 
     const cardContainer = $('<div></div>');
     const cardID = $('</p>');
     const cardTitle = $('</p>');
     const cardDesc = $('</p>');
     const cardDate = $('</p>');
-    
+    // Create delete button
+    const deleteBtn = $('<input/>', {
+        type: "button",
+        id: "delete-btn",
+        value: "Delete Task"
+    });
+    // Add text and class
     cardContainer.addClass('card');
-    cardTitle.addClass('card-title');
-    cardContainer.text(array.id);
-    cardTitle.text(array.title);
-    cardDesc.text(array.desc);
-    cardDate.text(array.date);
-
+    cardContainer.text(task.id);
+    cardTitle.text(task.title);
+    cardDesc.text(task.desc);
+    cardDate.text(task.date);
+    // Make card draggable
     cardContainer.draggable({
         opacity: 0.7,
         zIndex: 100,
-        // ? This is the function that creates the clone of the card that is dragged. This is purely visual and does not affect the data.
-        helper: function (e) {
-            // ? Check if the target of the drag event is the card itself or a child element. If it is the card itself, clone it, otherwise find the parent card  that is draggable and clone that.
-            const original = $(e.target).hasClass('ui-draggable')
-                ? $(e.target)
-                : $(e.target).closest('.ui-draggable');
-            // ? Return the clone with the width set to the width of the original card. This is so the clone does not take up the entire width of the lane. This is to also fix a visual bug where the card shrinks as it's dragged to the right.
+        // This is the function that creates the clone of the card that is dragged. This is purely visual and does not affect the data.
+        helper: function (event) {
+            // Check if the target of the drag event is the card itself or a child element. If it is the card itself, clone it, otherwise find the parent card  that is draggable and clone that.
+            const original = $(event.target).hasClass('ui-draggable')
+                ? $(event.target)
+                : $(event.target).closest('.ui-draggable');
+            // Return the clone with the width set to the width of the original card. This is so the clone does not take up the entire width of the lane. This is to also fix a visual bug where the card shrinks as it's dragged to the right.
             return original.clone().css({
                 width: original.outerWidth(),
             });
         }
     })
-
+    // Append text & button
     container.append(cardContainer);
     cardContainer.append(cardID);
     cardContainer.append(cardTitle);
     cardContainer.append(cardDesc);
     cardContainer.append(cardDate);
+    cardContainer.append(deleteBtn);
 }
 
 // Todo: create a function to handle adding a new task
 function handleAddTask(event) {
     event.preventDefault();
 
+    $(addTaskPrompt).dialog("close");
+
     console.log(taskTitle.val(), taskDate.val(), taskDesc.val())
     const title = taskTitle.val();
     const desc = taskDesc.val();
     const date = taskDate.val();
     const id = generateTaskId();
+    const status = "to-do";
 
     const taskObject = {
         id,
         title,
         desc,
         date,
+        status
     }
 
     toDoArr.push(taskObject);
@@ -95,8 +110,13 @@ function handleAddTask(event) {
 }
 
 // Todo: create a function to handle deleting a task
-function handleDeleteTask(event) {
+function handleDeleteTask(event, array, id) {
+    event.preventDefault();
+    console.log("delete");
 
+    const target = event.target;
+    const parent = target.parentNode;
+    parent.remove();
 }
 
 // Todo: create a function to handle dropping a task into a new status lane
@@ -116,10 +136,25 @@ $(document).ready(function () {
         position: { my: "center center", at: "center center", of: ".header-elements" }
     });
 
+    // Event on "Create Task" button
+    createTaskBtn.on('click', createTaskCard);
+
     // Event on "Add Task" button
-    createTaskBtn.on('click', function () {
-        createTaskCard();
+    addTaskBtn.on('click', handleAddTask);
+
+    swimLanes.droppable({
+        accept: '.draggable',
+        drop: handleDrop(),
     });
 
-    addTaskBtn.on('click', handleAddTask);
+    // swimLanes.each( function (lane) {
+    //     lane.on("dragover", (event) => {
+    //         event.preventDefault();
+    //         console.log("heard");
+    //     });
+    // });
+    
 });
+
+// Event listener for "Delete" button
+$(document).on("click", "#delete-btn", handleDeleteTask);
